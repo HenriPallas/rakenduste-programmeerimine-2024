b4b4a364465078d8a853664e1ee9134a9a333e97
Henri Pallas <hrp1609a@tlu.ee>
2024-09-09 17:15:30 +0300

    New react components

--- 09.09.2024/my-app/src/App.js
+++ 09.09.2024/my-app/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
-import Name from './components/Name'
+import Name from './components/Name';
+import React from 'react';
+import Counter from './components/Counter';
+import PropDrilling from './components/PropDrilling';
+
+// <React.Fragment></React.Fragment>
 
 function App() {
-  return <Name />;
+  return ( 
+    <>
+      <PropDrilling />
+      <Name title="Henri"/>
+      <Name />
+      <Counter />
+    </>
+  )
 }
 
 export default App;
--- 09.09.2024/my-app/src/components/Name.js
+++ 09.09.2024/my-app/src/components/Name.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Name = () => {
-    return <h1>Nimi</h1>
+/* const Name = (props) => {
+    console.log({ props })
+
+    const { title } = props;
+
+    return <h1>{title}</h1>
+}; */
+
+const Name = ({ title = "World" }) => {
+    return <h1>Hello {title}</h1>;
 };
 
 export default Name;
