2dd0b637540ceeb11e33a84fddce39361fbd6d6f
Henri Pallas <hrp1609a@tlu.ee>
2024-09-15 22:18:24 +0300

    "feat: Fixed dynamic buttons."

--- 09.09.2024/my-app/src/components/Counter.js
+++ 09.09.2024/my-app/src/components/Counter.js
@@ -4,17 +4,17 @@ const Counter = () => {
   const [counter, setCounter] = useState(0)
   // const [counter, setCounter] = React.useState(0);
 
-  const modifyCounter = () => setCounter(prevCounter => prevCounter + 1)
+  const modifyCounter = (amount) => setCounter(prevCounter => prevCounter + amount)
 
   return (
     <>
       <h1>{counter}</h1>
 
-      {[+1, +5, +50, -1, -5, -50].map(element => (
-        <button onClick={modifyCounter}>sync {element}</button>
+      {[+1, +5, +50, +100, -1, -5, -50, -100].map(element => (
+        <button onClick={() => modifyCounter(element)}>sync {element}</button>
       ))}
 
-      <button onClick={() => setTimeout(() => modifyCounter(), 2000)}>
+      <button onClick={() => setTimeout(() => modifyCounter(1), 2000)}>
         async +1
       </button>
     </>
